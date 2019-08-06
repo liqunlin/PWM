1604cd3d332961bcb56b43fe734188a7d90ab787
yanchao <845059920@qq.com>
2019-08-06 17:57:56 +0800

    提交docker-compose

--- install.sh
+++ install.sh
@@ -4,9 +4,22 @@ BASE_DIR=/opt/PWM
 cd $BASE_DIR && sed -i '/CONSUL_IP/d' PWM/settings.py
 cd $BASE_DIR && echo "CONSUL_IP = '$1'" >> PWM/settings.py
 
+cd $BASE_DIR && sed -i '/CONSUL_PORT/d' PWM/settings.py
+cd $BASE_DIR && echo "CONSUL_PORT = $7" >> PWM/settings.py
+
 cd $BASE_DIR && sed -i '/MONTIOR_REDIS/d' PWM/settings.py
 cd $BASE_DIR && echo "MONTIOR_REDIS = '$2'" >> PWM/settings.py
 
+cd $BASE_DIR && sed -i '/MONTIOR_PORT/d' PWM/settings.py
+cd $BASE_DIR && echo "MONTIOR_PORT = $6" >> PWM/settings.py
+
 cd $BASE_DIR && sed -i 's/10.50.182.65:9093/'$3'/g' PWM/settings.py
+
+
+cd $BASE_DIR && sed -i '/REDIS_IP/d' PWM/settings.py
+cd $BASE_DIR && echo "REDIS_IP = '$4'" >> PWM/settings.py
+
+cd $BASE_DIR && sed -i '/REDIS_PORT/d' PWM/settings.py
+cd $BASE_DIR && echo "REDIS_PORT = $5" >> PWM/settings.py
 
 cd $BASE_DIR && python manage.py runserver 0.0.0.0:888
