eba9681c94ea36e7d288f2abbb406e8597059967
yanchao <845059920@qq.com>
2019-08-09 16:14:20 +0800

    提交管理后台dockerfile

--- Dockerfile
+++ Dockerfile
@@ -15,7 +15,12 @@ ENV WX_URL="http://outrel.jieyue.com/outrel/api/externalplatform/interfaceRest/e
 ENV SMS_URL="http://172.18.100.168:20019/esbsmstrue/api/sms/general/send"
 ENV SENDMAIL_SMTP="smtp.jieyuechina.com:25"
 ENV SENDMAIL_SENDER="chaoyan1@jieyuechina.com"
-ENV SENDMAIL_SENDER_PASSWORD=""
+ENV SENDMAIL_SENDER_PASSWORD="aaa"
+ENV MYSQL_USER=""
+ENV MYSQL_PASSWORD=""
+ENV MYSQL_DB=""
+ENV MYSQL_IP=""
+ENV MYSQL_PORT=3306
 ADD PWM.tar.gz /opt/
 ADD 19.1.1.tar.gz /opt/
 ADD ez_setup.py /opt/
@@ -23,6 +28,7 @@ ADD install.sh /opt/
 WORKDIR $EZ_DIR
 RUN python ez_setup.py
 WORKDIR $PIP_DIR
+RUN yum install gcc gcc-c++ gcc-devel python-devel mysql-devel -y
 RUN python setup.py install
 WORKDIR $PROJECT_DIR
 RUN pip install -r requirements.txt
@@ -32,4 +38,4 @@ RUN chmod +x /opt/install.sh
 EXPOSE 888
 #CMD ["python manage.py runserver 0.0.0.0:888"]
 #ENTRYPOINT ["/bin/bash","-c"]
-ENTRYPOINT /opt/install.sh ${CONSUL_IP} ${MONTIOR_REDIS} ${ALERTMANAGER_HTTP} ${REDIS_IP} ${REDIS_PORT} ${MONTIOR_PORT} ${CONSUL_PORT} ${WX_URL} ${SMS_URL} ${SENDMAIL_SMTP} ${SENDMAIL_SENDER} ${SENDMAIL_SENDER_PASSWORD}
+ENTRYPOINT /opt/install.sh ${CONSUL_IP} ${MONTIOR_REDIS} ${ALERTMANAGER_HTTP} ${REDIS_IP} ${REDIS_PORT} ${MONTIOR_PORT} ${CONSUL_PORT} ${WX_URL} ${SMS_URL} ${SENDMAIL_SMTP} ${SENDMAIL_SENDER} ${SENDMAIL_SENDER_PASSWORD} ${MYSQL_USER} ${MYSQL_PASSWORD} ${MYSQL_DB} ${MYSQL_IP} ${MYSQL_PORT}
--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 BASE_DIR=/opt/PWM
 
-echo $1
+if [ ${16} ];
+then
+cd $BASE_DIR && /bin/cp -f PWM/settings_mysql.py PWM/settings.py
+cd $BASE_DIR && sed -i 's/pwm_db/'${15}'/g' PWM/settings.py
+cd $BASE_DIR && sed -i 's/pwm_user/'${13}'/g' PWM/settings.py
+cd $BASE_DIR && sed -i 's/pwm_passwd/'${14}'/g' PWM/settings.py
+cd $BASE_DIR && sed -i 's/pwm_db_host/'${16}'/g' PWM/settings.py
+cd $BASE_DIR && sed -i 's/pwm_db_port/'${17}'/g' PWM/settings.py
+fi
 
 cd $BASE_DIR && sed -i '/CONSUL_IP/d' PWM/settings.py
 cd $BASE_DIR && echo "CONSUL_IP = '$1'" >> PWM/settings.py
@@ -25,19 +33,19 @@ cd $BASE_DIR && sed -i '/REDIS_PORT/d' PWM/settings.py
 cd $BASE_DIR && echo "REDIS_PORT = $5" >> PWM/settings.py
 
 cd $BASE_DIR && sed -i '/WX_URL/d' PWM/settings.py
-cd $BASE_DIR && echo "WX_URL = '$8'" >> PWM/settings.py
+cd $BASE_DIR && echo "WX_URL = $8" >> PWM/settings.py
 
 cd $BASE_DIR && sed -i '/SMS_URL/d' PWM/settings.py
-cd $BASE_DIR && echo "SMS_URL = '$9'" >> PWM/settings.py
+cd $BASE_DIR && echo "SMS_URL = $9" >> PWM/settings.py
 
 cd $BASE_DIR && sed -i '/SENDMAIL_SMTP/d' PWM/settings.py
-cd $BASE_DIR && echo "SENDMAIL_SMTP = '${10}'" >> PWM/settings.py
+cd $BASE_DIR && echo "SENDMAIL_SMTP = ${10}" >> PWM/settings.py
 
 cd $BASE_DIR && sed -i '/SENDMAIL_SENDER/d' PWM/settings.py
-cd $BASE_DIR && echo "SENDMAIL_SENDER = '${11}'" >> PWM/settings.py
+cd $BASE_DIR && echo "SENDMAIL_SENDER = ${11}" >> PWM/settings.py
 
 cd $BASE_DIR && sed -i '/SENDMAIL_SENDER_PASSWORD/d' PWM/settings.py
-cd $BASE_DIR && echo "SENDMAIL_SENDER_PASSWORD = '${12}'" >> PWM/settings.py
+cd $BASE_DIR && echo "SENDMAIL_SENDER_PASSWORD = ${12}" >> PWM/settings.py
 
 
 cd $BASE_DIR && python manage.py runserver 0.0.0.0:888
